8526c55a4569af4fdbcaf0e42d0ad4ea9bb2c745
Payal <94229657+Payuu01@users.noreply.github.com>
2023-02-01 00:42:26 +0530

    Update doctor_schedule.php

--- admin/doctor_schedule.php
+++ admin/doctor_schedule.php
@@ -8,7 +8,7 @@ $object = new Appointment;
 
 if(!$object->is_login())
 {
-    header("location:".$object->base_url."admin");
+    header("location:".$object->base_url."");
 }
 
 include('header.php');
